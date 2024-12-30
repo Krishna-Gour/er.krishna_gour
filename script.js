7382cb8a869cc2a427469f5812d258acec0866a0
Krishna Gour <113084783+Krishna-Gour@users.noreply.github.com>
2024-12-30 12:56:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
-<script>
-  function toggleMenu() {
-    const navLinks = document.getElementById('nav-links');
-    navLinks.classList.toggle('active');
-  }
-</script>
+console.log('Script loaded'); // Check if the script is loaded
+
+function toggleMenu() {
+  console.log('Menu toggled'); // Debug when menu is clicked
+
+  const navLinks = document.getElementById('nav-links');
+  navLinks.classList.toggle('active');
+}
